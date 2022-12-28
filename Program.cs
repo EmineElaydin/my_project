a1338eff67f7fab3ee909f126eaa2e0ff017bb23
unknown <emineelaydin2@gmail.com>
2022-12-28 15:39:02 +0400

    28.12

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿//Console.WriteLine("adınızı ve soyadınızı daxil edin:");
+//string ad= Console.ReadLine();  
+//string soyad=Console.ReadLine();    
+//Console.WriteLine("xoş gəlmisiniz" + ad + " " + soyad);
+
+//EYNİ KODU TƏKRAR QISA ŞƏKİLDƏ YAZMAQ
+
+
+//int a = 8;
+//int b = 2;
+//int cəm = a + b;
+//int fərq = a - b;
+//int hasil = a * b;
+//int bölmə = a / b;
+//int faiz = a % b;
+//Console.WriteLine(cəm + " /n "  + fərq + " "+ hasil );
+//a = a +1 ;
+//Console.WriteLine(a); 
+//Console.WriteLine(a);
+
+
+//MƏNİMSƏTMƏ
+
+
+//int x = 8;
+//Console.WriteLine(x);
+//x += 3;
+//Console.WriteLine(x);
+
+
+//MÜQAYİSƏ
+
+//int rəqəm = 6;
+//int rəqəm1 =9;
+//Console.WriteLine(rəqəm  <= rəqəm1);
+
+
+//MENTIQI OPERATORLAR
+
+//int R = 1;
+//int Z = 2; ;
+//Console.WriteLine( R +$" "+ Z  );
+
+
+
+
+
+
+//STRİNGS
+
+
+//string s = "salam";
+//Console.WriteLine(s [0] );
+//Console.WriteLine( s[1] );  
+//BURDA S HƏRFİNİ NECƏ BÖYÜK YAZA BİLƏRƏM
+string ad = "emine";
+string soyad= "eliyeva";
+string.Concat(ad, soyad);
+Console.WriteLine($"xoşgeldiniz" + " " + [ ad ] ) ;
+//sətirdən sətirə keçmir /n ilə.
+
+//RİYAZİ OPERATORLAR
+
+//int maxsimum = Math.Max(5, 10);
+//Console.WriteLine(maxsimum);
+
+
+//double kokaltı = Math.Sqrt(64);
+//Console.WriteLine(kokaltı);
+////MODUL
+
+
+//Console.WriteLine(Math.Abs(-6));
+//Console.WriteLine(Math.Round (3.8));
+
+
+
